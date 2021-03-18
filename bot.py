4304040f0236b5ab5807cd5649ade5eb11e83afe
funch11 <79532572+funch11@users.noreply.github.com>
2021-03-18 22:58:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
+import os
 
-api = "ODIwNTc5MjMwMTIzMDMyNTc2.YE3N9g.HChUBr4u2KeVlT32bqea4yutRBY"
 client = commands.Bot(command_prefix='*')
 
  
@@ -56,7 +56,7 @@ async def w_error(ctx: commands.Context, error:commands.CommandInvokeError):
         emd = discord.Embed(title=" :x:  City not found", description = f"**Requseted by {ctx.author.mention}**")
         await ctx.send(embed = emd)
 
-client.run(api)
+token = os.environ.get("BOT_TOKEN")
 
 
 
